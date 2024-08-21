3372baf49e735be75c84a67f722fae377e79b25e
MiloGit <milolemosa01@outlook.com>
2024-08-20 19:23:21 -0500

    Deteccion Filas y Columnas

--- src/gpio_config.cpp
+++ src/gpio_config.cpp
@@ -2,22 +2,41 @@
 #include "gpio_config.h"
 #include <vector>
 
-void setupGPIOs(std::vector<gpio_num_t>& ROWS, std::vector<gpio_num_t>& COLS){
+void setupGPIOs(std::vector<gpio_num_t>& ROWS, std::vector<gpio_num_t>& COLS, bool SaveState){
     gpio_config_t config;
-    //Filas como Salidas
-    for(size_t i = 0; i<ROWS.size(); i++){
-        config.mode = GPIO_MODE_OUTPUT;
-        config.pin_bit_mask = (1ULL << ROWS[i]);
-        config.intr_type = GPIO_INTR_DISABLE;
-        config.pull_down_en = GPIO_PULLDOWN_DISABLE;
-        config.pull_up_en = GPIO_PULLUP_DISABLE;
-        gpio_config(&config);
-    }
-    //Columnas como Entradas
-    for(size_t j=0; j<COLS.size(); j++){
-        config.mode = GPIO_MODE_INPUT;
-        config.pin_bit_mask = (1ULL << COLS[j]);
-        config.pull_down_en = GPIO_PULLDOWN_ENABLE;
-        gpio_config(&config);
+    if(SaveState){
+        //Filas como Salidas
+        for(size_t i = 0; i<ROWS.size(); i++){
+            config.mode = GPIO_MODE_OUTPUT;
+            config.pin_bit_mask = (1ULL << ROWS[i]);
+            config.intr_type = GPIO_INTR_DISABLE;
+            config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+            config.pull_up_en = GPIO_PULLUP_DISABLE;
+            gpio_config(&config);
+        }
+        //Columnas como Entradas
+        for(size_t j=0; j<COLS.size(); j++){
+            config.mode = GPIO_MODE_INPUT;
+            config.pin_bit_mask = (1ULL << COLS[j]);
+            config.pull_down_en = GPIO_PULLDOWN_ENABLE;
+            gpio_config(&config);
+        }
+    }else{
+        //Columnas como Salidas
+        for(size_t i = 0; i<COLS.size(); i++){
+            config.mode = GPIO_MODE_OUTPUT;
+            config.pin_bit_mask = (1ULL << COLS[i]);
+            config.intr_type = GPIO_INTR_DISABLE;
+            config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+            config.pull_up_en = GPIO_PULLUP_DISABLE;
+            gpio_config(&config);
+        }
+        //Filas como Entradas
+        for(size_t j=0; j<ROWS.size(); j++){
+            config.mode = GPIO_MODE_INPUT;
+            config.pin_bit_mask = (1ULL << ROWS[j]);
+            config.pull_down_en = GPIO_PULLDOWN_ENABLE;
+            gpio_config(&config);
+        }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -41,15 +41,21 @@ int timerID = 1;
 
 static const char* TAG = "Main";
 
+bool SaveState = true;
+
 //Vector guardar las salidas
 uint8_t outRows = 0;
+uint8_t outCols = 0;
 
 //Matriz de detección
-std::vector<std::vector<uint8_t>> matChess(8, std::vector<uint8_t>(8));
+std::vector<std::vector<uint8_t>> matChessRowsIn(8, std::vector<uint8_t>(8));   //Rows como Entradas - ESP32
+std::vector<std::vector<uint8_t>> matChessColsIn(8, std::vector<uint8_t>(8));   //Cols como Entradas - ESP32
+std::vector<std::vector<uint8_t>> matChessConcat(8, std::vector<uint8_t>(8));   //Concatenación de Matrices
 
 //Prototipo de Funciones
 void activacionSalidas(std::vector<gpio_num_t> Salidas);
-void formarMatrix(std::vector<gpio_num_t> vector);
+void formarMatrix(void);
+void ConcatArrays(void);
 void imprimirVector(std::vector<uint8_t> vector);
 void imprimirMatrix(std::vector<std::vector<uint8_t>> matriz);
 
@@ -58,13 +64,9 @@ void imprimirMatrix(std::vector<std::vector<uint8_t>> matriz);
 //Función de Interrupción Lectura
 void ISR_funcionLectura(TimerHandle_t timer){
     ESP_LOGI(TAG, "--------MatrixChess---------");
-    //Lectura Columnas
-    //ESP_LOGI(TAG, "Valor de Fila: %u", outRows);
-    //Formar Matriz de Detección
-    formarMatrix(COLS);
-    //ESP_LOGI(TAG, "matChess[0][0]: %u", matChess[0][0]);
-    //imprimirVector(lecCols);
-    imprimirMatrix(matChess);
+    formarMatrix();//Formar Matriz de Detección
+    ConcatArrays();//Concatenación de arrays de Detección
+    //imprimirMatrix(matChessConcat);
 }
 
 //-----------------------------MAIN-----------------------------
@@ -72,10 +74,10 @@ extern "C" void app_main();
 
 void app_main(){
     //Setup
-    setupGPIOs(ROWS, COLS);
+    setupGPIOs(ROWS, COLS, SaveState);
     ESP_LOGI(TAG, "GPIOS Inicializados");
 
-    //Configuracion Timer
+    //Configuración Timer
     ESP_LOGI(TAG, "Inicializacion de Timer");
     timer_isr = xTimerCreate("Timer",
                              pdMS_TO_TICKS(interval),
@@ -94,38 +96,72 @@ void app_main(){
 
     //Bucle 
     while(1){
-        activacionSalidas(ROWS);
+        //Activación de Filas y Columnas
+        (SaveState) ? activacionSalidas(ROWS) : activacionSalidas(COLS);
     }   
 }
 
 //-----------------------------FUNCIONES-----------------------------
-/*Funcion para activar Salidas*/
+/*Función para activar Salidas*/
 void activacionSalidas(std::vector<gpio_num_t> Salidas){
     for(size_t i=0; i<Salidas.size(); i++){
-        gpio_set_level(Salidas[i], 1);                          //Activacion de la salida actual
-        //Guardamos la posicion de la activacion actual
-        outRows = i;
+        gpio_set_level(Salidas[i], 1);                          //Activación de la salida actual
+        //Guardamos la posición de la activación actual
+        if(SaveState){
+            outRows = i;
+        }else{
+            outCols = i;
+        }
         gpio_set_level((i==0) ? Salidas[7] : Salidas[i-1], 0);  //Desactivamos la salida anterior
 
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(150));
     }
     gpio_set_level(Salidas[7], 0);
+    //Cambiamos E/S
+    SaveState = !SaveState;
+    setupGPIOs(ROWS, COLS, SaveState);
 }
 
-/*Funcion para armar la matrixChess con las detecciones de los sensores y las filas*/
-void formarMatrix(std::vector<gpio_num_t> vector){
-    for(size_t i=0; i<vector.size(); i++){
-        matChess[outRows][i] = gpio_get_level(vector[i]);
+/*Función para armar la matrixChess con las detecciones de los sensores y las filas*/
+void formarMatrix(void){
+    if(SaveState){
+        for(size_t i=0; i<ROWS.size(); i++){
+            //Filas -> Salidas
+            //Columnas -> Entradas
+            matChessColsIn[outRows][i] = gpio_get_level(COLS[i]);
+        }
+        imprimirMatrix(matChessColsIn);
+    }else{
+        for(size_t i=0; i<COLS.size(); i++){
+            //Filas -> Entradas
+            //Columnas -> Salidas
+            matChessRowsIn[i][outCols] = gpio_get_level(ROWS[i]);
+        }
+        imprimirMatrix(matChessRowsIn);
     }
 }
 
-/*Funcion para imprimir vectores*/
+/*Función para concatenar la detección*/
+void ConcatArrays(void){
+    //Recorremos las matrices
+    for(size_t i=0; i<matChessRowsIn.size(); i++){  //Recorrermos Filas
+        for(size_t j=0; j<matChessRowsIn[0].size(); j++){   //Recorremos Columnas
+            if((matChessRowsIn[i][j] == 1) || (matChessColsIn[i][j] == 1)){
+                matChessConcat[i][j] = 1;
+            }else{
+                matChessConcat[i][j] = 0;
+            }
+        }
+    }
+}
+
+/*Función para imprimir vectores*/
 void imprimirVector(std::vector<uint8_t> vector){
     ESP_LOGI(TAG, "F: %u C: %u %u %u %u %u %u %u %u", vector[0], vector[1], vector[2],
              vector[3], vector[4], vector[5], vector[6], vector[7], vector[8]);
 }
 
-/*Funcion para imprimir la matriz*/
+/*Función para imprimir la matriz*/
 void imprimirMatrix(std::vector<std::vector<uint8_t>> matriz){
     ESP_LOGI(TAG, "\n%u %u %u %u\n%u %u %u %u", 
                    matriz[0][0], matriz[0][1], matriz[0][2], matriz[0][3], 
